0f190446ca47d7ae1c60567c352d36d4977f1b28
Lode <lode@codechamp.be>
2019-08-21 11:56:57 +0200

    Added DI.Core.StructureMap

---
+++ DI/Ioc samples/DI.Core.StructureMap/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+using System;
+
+namespace DI.Core.StructureMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection();
+
+            var container = new Container();
+            container.Configure(config =>
+            {
+                config.Scan(_ =>
+                {
+                    _.AssemblyContainingType(typeof(Commerce));
+                    _.WithDefaultConventions();
+                });
+
+                config.Populate(serviceProvider);
+            });
+
+            Console.WriteLine(".NET Core DI Container Example");
+            Console.WriteLine();
+
+            OrderInfo orderInfo = new OrderInfo()
+            {
+                CustomerName = "Miguel Castro",
+                Email = "miguel@dotnetdude.com",
+                Product = "Laptop",
+                Price = 1200,
+                CreditCard = "1234567890"
+            };
+
+            Console.WriteLine("Production:");
+            Console.WriteLine();
+
+            Commerce commerce = container.GetInstance<Commerce>();
+            commerce.ProcessOrder(orderInfo);
+
+            Console.WriteLine();
+            Console.WriteLine("Press [Enter] to exit...");
+            Console.ReadLine();
+        }
+    }
+}
