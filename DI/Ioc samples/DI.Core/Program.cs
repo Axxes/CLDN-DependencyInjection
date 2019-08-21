c000a0117e5a77ac22ebd852ca1296cd8f18ef48
Lode <lode@codechamp.be>
2019-08-21 11:51:27 +0200

    Log change

--- DI/Ioc samples/DI.Core/Program.cs
+++ DI/Ioc samples/DI.Core/Program.cs
@@ -9,8 +9,6 @@ namespace DI.Core
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             var serviceProvider = new ServiceCollection()
                 .AddTransient<IBillingProcessor, BillingProcessor>()
                 .AddTransient<ICustomer, Customer>()
@@ -19,7 +17,7 @@ namespace DI.Core
                 .AddTransient<Commerce>()
                 .BuildServiceProvider();
 
-            Console.WriteLine("NInject DI Container Example");
+            Console.WriteLine(".NET Core DI Container Example");
             Console.WriteLine();
 
             OrderInfo orderInfo = new OrderInfo()
