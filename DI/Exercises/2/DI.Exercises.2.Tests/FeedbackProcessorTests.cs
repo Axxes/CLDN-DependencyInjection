35101c683ef4a86efb80c90ad7b5246a5fcb4e8d
Lode <lode@codechamp.be>
2019-08-22 18:25:59 +0200

    basic test added

--- DI/Exercises/2/DI.Exercises.2.Tests/FeedbackProcessorTests.cs
+++ DI/Exercises/2/DI.Exercises.2.Tests/FeedbackProcessorTests.cs
@@ -13,7 +13,6 @@ namespace DI.Exercises._2.Tests
     public class FeedbackProcessorTests
     {
         private IServiceCollection _serviceCollection;
-        private IServiceProvider _serviceProvider;
 
         [SetUp]
         public void Setup()
@@ -22,31 +21,36 @@ namespace DI.Exercises._2.Tests
 
             // Inject dependencies
             _serviceCollection.AddSingleton <IFeedbackProcessor, FeedbackProcessor>();
-            // serviceCollection.AddScoped<IFakeDatabase, ?>();
-            // serviceCollection.AddTransient<INotifier, ?>();
-
-            _serviceProvider = _serviceCollection.BuildServiceProvider();
         }
 
         [Test]
-        public async Task Test1()
+        public async Task AssertNotifiyCalledThreeTimesPerBatch()
         {
-            var processor = _serviceProvider.GetService<IFeedbackProcessor>();
-
             var mr = new MockRepository(MockBehavior.Strict);
-
-            var database = mr.Create<IFakeDatabase>();
-            database.Setup(p => p.Save(It.IsAny<Feedback>()));
-            _serviceCollection.AddScoped(_ => database.Object);
 
             var notifier = mr.Create<INotifier>();
             notifier.Setup(p => p.Notify(It.IsAny<Feedback>()));
             _serviceCollection.AddTransient(_ => notifier.Object);
 
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            var processor = serviceProvider.GetService<IFeedbackProcessor>();
+
             await processor.StartAsync(CancellationToken.None);
 
-            database.Verify(p => p.Save(It.IsAny<Feedback>()), Times.Exactly(3));
-            Assert.Pass();
+            processor.Queue.Add(new Feedback());
+            processor.Queue.Add(new Feedback());
+            processor.Queue.Add(new Feedback());
+
+            await Task.Delay(500);
+            notifier.Verify(p => p.Notify(It.IsAny<Feedback>()), Times.Exactly(3));
+
+            processor.Queue.Add(new Feedback());
+            processor.Queue.Add(new Feedback());
+            processor.Queue.Add(new Feedback());
+            await Task.Delay(500);
+
+            notifier.Verify(p => p.Notify(It.IsAny<Feedback>()), Times.Exactly(6));
         }
     }
 }
