825061afb5d3f91d198f05c3375de031099a17b6
Lode Kennes <lode.kennes@axxes.com>
2020-08-20 16:19:28 +0200

    Removed implementation container

--- DI/Ioc samples/src/Essentials/DI.Essentials.PoorMansContainer/DIContainer/Container.cs
+++ DI/Ioc samples/src/Essentials/DI.Essentials.PoorMansContainer/DIContainer/Container.cs
@@ -15,68 +15,34 @@ namespace DI.Essentials.PoorMansContainer.DIContainer
         public void Register<T, U>()
             where U : class, new()
         {
-            var abstractionType = typeof(T);
-            var concreteType = typeof(U);
-
-            Register(abstractionType, concreteType);
+            throw new NotImplementedException();
         }
 
         public void Register(Type abstractionType, Type concreteType)
         {
-            if (!abstractionType.IsInterface)
-                throw new ApplicationException("First generic argument must be an interface type.");
-
-            _Registrations.Add(new ContainerItem() { AbstractionType = abstractionType, ConcreteType = concreteType });
+            throw new NotImplementedException();
         }
 
         private readonly List<ContainerItem> _Registrations;
 
         public T CreateType<T>() where T : class
         {
-            var type = typeof(T);
-
-            return (T)GetConcreteType(type);
+            throw new NotImplementedException();
         }
 
         public object CreateType(Type type)
         {
-            return GetConcreteType(type);
+            throw new NotImplementedException();
         }
 
         private object GetConcreteType(Type typeToResolve)
         {
-            var containerItem = _Registrations.Where(item => item.AbstractionType.Equals(typeToResolve)).FirstOrDefault();
-            if (containerItem != null)
-                return GetTypeInstance(containerItem.ConcreteType);
-            else
-                return GetTypeInstance(typeToResolve);
+            throw new NotImplementedException();
         }
 
         private object GetTypeInstance(Type type)
         {
-            object instance = null;
-
-            var constructors = type.GetConstructors();
-            if (constructors.Length > 0)
-            {
-                var constructor = constructors[0];
-
-                var constructorArguments = new List<object>();
-                var parameters = constructor.GetParameters();
-                foreach (var parameter in parameters)
-                {
-                    object parameterInstance = null;
-                    if (parameter.ParameterType.IsInterface)
-                        // recursion happens here
-                        parameterInstance = GetConcreteType(parameter.ParameterType);
-
-                    constructorArguments.Add(parameterInstance);
-                }
-
-                instance = Activator.CreateInstance(type, constructorArguments.ToArray());
-            }
-
-            return instance;
+            throw new NotImplementedException();
         }
     }
 }
